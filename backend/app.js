29cf3360f0ba61f2799ee0f5ec6ef0e5da12a52b
TaiH1999 <huynguyen090999@gmail.com>
2024-08-26 22:47:48 +0700

    app.js

--- backend/app.js
+++ backend/app.js
@@ -29,24 +29,6 @@ app.post('/orders', async (req, res) => {
       .json({ message: 'Missing data.' });
   }
 
-  // if (
-  //   orderData.customer.email === null ||
-  //   !orderData.customer.email.includes('@') ||
-  //   orderData.customer.name === null ||
-  //   orderData.customer.name.trim() === '' ||
-  //   orderData.customer.street === null ||
-  //   orderData.customer.street.trim() === '' ||
-  //   orderData.customer['postal-code'] === null ||
-  //   orderData.customer['postal-code'].trim() === '' ||
-  //   orderData.customer.city === null ||
-  //   orderData.customer.city.trim() === ''
-  // ) {
-  //   return res.status(400).json({
-  //     message:
-  //       'Missing data: Email, name, street, postal code or city is missing.',
-  //   });
-  // }
-
   const newOrder = {
     ...orderData,
     id: (Math.random() * 1000).toString(),
